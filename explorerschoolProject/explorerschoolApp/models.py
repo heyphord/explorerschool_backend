3f5262e1f4cd8d84777851c803553675440bf42c
heyphord <apps.heyphord@gmail.com>
2021-07-15 13:16:31 +0000

    student and  tutor model done

--- explorerschoolProject/explorerschoolApp/models.py
+++ explorerschoolProject/explorerschoolApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=100)
+    department = models.CharField(max_length=40)
+
+    class Meta:
+        ordering = ['first_name']
+
+    def __str__(self):
+        return self.first_name
+
+
+class Tutor(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    students = models.ManyToManyField(Student)
+
+    class Meta:
+        ordering = ['first_name']
+
+    def __str__(self):
+        return self.first_name
+
+    
