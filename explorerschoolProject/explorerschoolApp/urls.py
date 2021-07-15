2d2f630a847c51be90cbd9bcf92a19dd06bd870a
heyphord <apps.heyphord@gmail.com>
2021-07-15 19:30:55 +0000

    tutor url updated

--- explorerschoolProject/explorerschoolApp/urls.py
+++ explorerschoolProject/explorerschoolApp/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import StudentAPIVIEW , StudentDetailsAPIVIEW
+from .views import StudentAPIVIEW , StudentDetailsAPIVIEW , TutorAPIVIEW, TutorDetailsAPIVIEW
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('students/',StudentAPIVIEW.as_view()),
     path('student/<int:id>', StudentDetailsAPIVIEW.as_view()),
+    path('tutors/',TutorAPIVIEW.as_view()),
+    path('tutor/<int:id>', TutorDetailsAPIVIEW.as_view()),
     
 ]
