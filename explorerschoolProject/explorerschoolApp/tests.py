a0003f58c8b88fe5afd863ea10829bd331e6f695
heyphord <apps.heyphord@gmail.com>
2021-07-18 22:00:15 +0000

    Students test cases

--- explorerschoolProject/explorerschoolApp/tests.py
+++ explorerschoolProject/explorerschoolApp/tests.py
@@ -24,7 +24,6 @@ class TutorTestCase(TestCase):
         {
             "first_name": "hayford2",
             "last_name" : "ansah2",
-            "department": "I.T",
             "email":"hayford2@gamil.com"
 
         })
@@ -61,7 +60,7 @@ class TutorTestCase(TestCase):
         student = Student( first_name ="Hayford", last_name="Ansah", department="Computer science",  email="student@gmail.com")
         student.save()
         c = Client()
-        
+
         response = c.post('/student-tuttor/assign/',
         {
             "tutor_id" : tutor.id,
@@ -72,3 +71,55 @@ class TutorTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         
 
+class StudentTestCase(TestCase):
+    def test_get_students(self):
+        c = Client()
+        response = c.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_get_student(self):
+        student = Student( first_name ="Hayford", last_name="Ansah", department="COmputer science",  email="o.ansah.hayford@gmail.com")
+        student.save()
+
+        c = Client()
+        response = c.get('/student/'+ str(student.id))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_create_student(self):
+        
+        c = Client()
+        response = c.post('/students/' ,
+        {
+            "first_name": "hayford2",
+            "last_name" : "ansah2",
+            "department": "I.T",
+            "email":"hayford2@gamil.com"
+
+        })
+        self.assertEqual(response.status_code, 201)
+        
+    def test_edit_student(self):
+        student = Student( first_name ="Hayford", last_name="Ansah", department="I.T",  email="o.ansah.hayford@gmail.com")
+        student.save()
+
+        c = Client()
+        response = c.put('/student/'+ str( student.id) ,
+        {
+            "first_name": "hayfordEdited",
+            "last_name" : "ansahEdited",
+            "department" : "Computer science",
+            "email":"hayford@gamil.com"
+
+        },
+        content_type='application/json')
+        # print( response.content)
+        self.assertEqual(response.status_code, 201)
+        
+    def test_delete_student(self):
+        student = Student( first_name ="Hayford", last_name="Ansah",  email="o.ansah.hayford@gmail.com")
+        student.save()
+
+        c = Client()
+        response = c.delete('/student/'+ str( student.id) )
+        # print( response.content)
+        self.assertEqual(response.status_code, 204)
